14328091582f4496ffe8a2f42847585f5ece292f
santensuru <djuned.ong@gmail.com>
2015-04-24 14:38:57 +0700

    init

---
+++ request.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>request CA</title>
+	</head>
+	<body>
+		<h1>CA</h1>
+		
+		<!-- content area -->
+		<div>
+		<?php
+			echo "helo";
+			header("Location: oke.html");
+		?>
+		</div>
+
+	</body>
+</html>
